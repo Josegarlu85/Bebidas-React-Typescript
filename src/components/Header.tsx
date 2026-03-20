093de4a35e49c441e1ca7d738b857bc4b9fe2dda
D4DOM\josgarcia <joseja>
2026-03-20 08:22:59 +0100

    Primer commit: Proyecto Bebidas

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+
+import { useMemo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
+
+export default function Header() {
+  const {pathname} = useLocation()
+
+  const isHome = useMemo(() => pathname === '/', [pathname])
+
+  return (
+ <header className={isHome ? 'bg-header bg-center bg-cover min-h-screen' : 'bg-slate-800'}>
+    <div className="mx-auto container px-5 py-16">
+        <div className=" flex justify-between items-center">
+            <div>
+                <img className="w-32" src="/logo.svg" alt="logotipo" />
+            </div>
+            <nav className="flex gap-4">
+                <NavLink to="/" className={({isActive}) => isActive ? 'text-yellow-500 uppercase font-bold' : 'text-white uppercase font-bold'}>
+                Inicio</NavLink>
+                <NavLink to="/favoritos" className={({isActive}) => isActive ? 'text-yellow-500 uppercase font-bold' : 'text-white uppercase font-bold'}>
+                Favoritos</NavLink>
+            </nav>
+        </div>
+
+        {isHome && (
+            <form className="md:w-1/2 2xl:w-1/3 my-32 p-10
+            rounded:-lg bg-orange-400 shadow space-y-5  ">
+                <div>
+                    <label htmlFor="Ingrediente" className="block text-white uppercase font-extrabold text-lg">
+                        Nombre o ingredientes
+                    </label>
+                    <input id="ingrediente" type="text" name="ingredient"
+                    className="p-3 w-full bg-white rounded-lg focus:outline-none"
+                    placeholder="Nombre o ingrediente: Ej (Vodka, tequila....)" /> 
+                </div>
+                  <div>
+                    <label htmlFor="Ingrediente" className="block text-white uppercase font-extrabold text-lg">
+                        Categoria
+                    </label>
+                    <select  id="ingrediente"  name="ingredient"
+                    className="p-3 w-full bg-white rounded-lg focus:outline-none"
+                   >
+                        <option value="">-- Selecciona una categoria --</option>
+                    </select>
+                </div>
+                <input type="submit" value="Buscar recetas"
+                className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white
+                font-extrabold w-full p-2 rounded-lg uppercase" />
+            </form>
+        ) }
+    </div>
+        
+ </header>
+  )
+}
