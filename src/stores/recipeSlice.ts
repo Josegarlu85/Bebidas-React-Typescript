8fa0a3f27ea117d0c281959a5ec6269281eb787e
D4DOM\josgarcia <joseja>
2026-03-23 10:46:47 +0100

    feat: integrate modal UI and display recipe details

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,13 +1,16 @@
 import type { StateCreator } from "zustand"
 import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
-import type { Categories, Drink, Drinks, SearchFilter } from "../types"
+import type { Categories, Drink, Drinks, Recipe, SearchFilter } from "../types"
 
 export type RecipesSliceType = {
     categories: Categories
     drinks : Drinks
+    selectedRecipe: Recipe
+    modal: boolean
     fetchCategories: () => Promise<void>
     searchRecipes: (searchFilters: SearchFilter) => Promise<void>
     selectRecipe: (id: Drink["idDrink"]) => Promise<void>
+    closeModal:() => void
 }
 
 export const createRecipeSlice: StateCreator<RecipesSliceType> = (set) => ({
@@ -17,6 +20,8 @@ export const createRecipeSlice: StateCreator<RecipesSliceType> = (set) => ({
     drinks:{
         drinks: []
     },
+    selectedRecipe:{} as Recipe,
+    modal: false,
     fetchCategories: async () => {
         const data = await getCategories()
 
@@ -34,6 +39,15 @@ export const createRecipeSlice: StateCreator<RecipesSliceType> = (set) => ({
     },
     selectRecipe: async (id) => {
         const selectedRecipe = await getRecipeById(id)
-        console.log(selectedRecipe)
+        set({
+            selectedRecipe,
+            modal: true
+        })
+    },
+    closeModal: () => {
+        set({
+            modal:false,
+            selectedRecipe: {} as Recipe
+        })
     }
 })
