bec2da1d5cc55a1451cf672df0e2f224f0875052
D4DOM\josgarcia <joseja>
2026-03-23 11:50:04 +0100

    feat: implement add/remove favorites functionality

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,6 +8,7 @@ export default function Modal() {
   const modal = useAppStore(state => state.modal)
   const closeModal = useAppStore(state => state.closeModal)
   const selectedRecipe = useAppStore(state => state.selectedRecipe)
+  const handleClickFavorite = useAppStore(state => state.handleClickFavorite)
 
   const renderIngredients = ()=> {
     const ingredients : JSX.Element[] = []
@@ -79,6 +80,7 @@ export default function Modal() {
                         <button type='button'
                         className='full rounded bg-orange-600 p-3 font-bold
                         uppercase text-white shadow hover:bg-orange-500'
+                        onClick={() => handleClickFavorite(selectedRecipe)}
                         >Agregar a Favoritos</button>
                     </div>
                 </Dialog.Panel>
